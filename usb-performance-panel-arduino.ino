8c5832e89b85e5160270f27f646cf6301ee48bf0
fhh200000 <1041154549@qq.com>
2022-02-06 19:43:03 +0800

    Update:Initial commit.

---
+++ usb-performance-panel-arduino.ino
@@ -0,0 +1,46 @@
+#include <PluggableUSB.h>
+typedef enum {
+    DATA_CPU_USAGE=0x01,
+    DATA_RAM_USAGE=0x02,
+//...
+    DATA_MAX_OUT  =0x80
+}AvailableDataChannels;
+class UsbPerformancePanel:public PluggableUSBModule {
+    private:
+        uint8_t epType[1] = {EP_TYPE_CONTROL};
+        uint8_t availChn[1] = {(DATA_CPU_USAGE|DATA_RAM_USAGE)};
+    public:
+        UsbPerformancePanel():PluggableUSBModule(1, 1, epType) {
+            PluggableUSB().plug(this);
+        }
+        bool setup(USBSetup& setup);
+        int getInterface(uint8_t* interfaceCount);
+        int getDescriptor(USBSetup& setup);
+};
+bool UsbPerformancePanel::setup(USBSetup& st)
+{
+    if (pluggedInterface != st.wIndex) {
+        return false;
+    }
+    uint8_t request = st.bRequest;
+    uint8_t requestType = st.bmRequestType;
+}
+int UsbPerformancePanel::getInterface(uint8_t* interfaceCount)
+{
+    
+}
+int UsbPerformancePanel::getDescriptor(USBSetup& st)
+{
+    
+}
+void setup()
+{
+    // put your setup code here, to run once:
+
+}
+
+void loop()
+{
+    // put your main code here, to run repeatedly:
+
+}
